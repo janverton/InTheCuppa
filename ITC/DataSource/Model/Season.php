e7a047b48f9d5ef97458b123d9ed643c9a8f5e68
alifhughes <alistair.hughes@students.plymouth.ac.uk>
2015-09-20 13:58:33 +0100

    Season class (unfinished)

---
+++ ITC/DataSource/Model/Season.php
@@ -0,0 +1,20 @@
+<?php
+
+// Define namespaces
+namespace ITC\DataSource\Model;
+
+class Season extends AbstractGame
+{
+    
+
+    public function getScore(User $user) {
+        // Observer pattern implemented
+        // Notified when game is ended
+    }
+
+    public function getScoreSheet() {
+        
+    }
+
+}
+
